36aaca8878a67dcf41c6d6b30c174e7d1bfb2cff
Nick Beattie <nick@nickbytes.com>
2019-01-08 10:59:39 -0500

    adding styled components? why not

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,36 @@
 import Layout from "../components/Layout";
+import styled from "styled-components";
+
+const Headline = styled.h1`
+  font-size: 48px;
+  @media (min-width: 768px) {
+    font-size: 64px;
+  }
+`;
+
+const SubTitle = styled.h2`
+  font-weight: normal;
+  margin-bottom: 50px;
+`;
+
+const Section = styled.section`
+  max-width: 608px;
+`;
 
 export default () => (
   <Layout>
     <main>
-      <section>
-        <h1>Sunny Day</h1>
+      <Section>
+        <Headline>Sunny Day</Headline>
+        <SubTitle>
+          A digital studio building maintainable and performant applications,
+          platforms, and websites.
+        </SubTitle>
         <p>
           Want to work with us? email{" "}
           <a href="mailto:hi@sunnday.tech">hi@sunnyday.tech</a>
         </p>
-      </section>
-      <style jsx>
-        {`
-          h1 {
-            font-size: 64px;
-          }
-        `}
-      </style>
+      </Section>
     </main>
   </Layout>
 );
