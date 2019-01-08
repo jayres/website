4d2ca839982589040a5734e3e9ac7c5cf43642dc
Nick Beattie <nick@nickbytes.com>
2019-01-08 10:19:03 -0500

    initial re-arch commit

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+export default () => (
+  <main>
+    <section>
+      <h1>Sunny Day</h1>
+      <p>
+        Want to work with us? email{" "}
+        <a href="mailto:hi@sunnday.tech">hi@sunnyday.tech</a>
+      </p>
+    </section>
+    <style jsx>
+      {`
+        h1 {
+          font-size: 64px;
+        }
+      `}
+    </style>
+  </main>
+);
