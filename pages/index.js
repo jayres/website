e7a7bc4ad870b28929f48bdee377699d1f252246
Nick Beattie <nick@nickbytes.com>
2019-01-08 10:37:29 -0500

    added dev script

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,22 @@
+import Layout from "../components/Layout";
+
 export default () => (
-  <main>
-    <section>
-      <h1>Sunny Day</h1>
-      <p>
-        Want to work with us? email{" "}
-        <a href="mailto:hi@sunnday.tech">hi@sunnyday.tech</a>
-      </p>
-    </section>
-    <style jsx>
-      {`
-        h1 {
-          font-size: 64px;
-        }
-      `}
-    </style>
-  </main>
+  <Layout>
+    <main>
+      <section>
+        <h1>Sunny Day</h1>
+        <p>
+          Want to work with us? email{" "}
+          <a href="mailto:hi@sunnday.tech">hi@sunnyday.tech</a>
+        </p>
+      </section>
+      <style jsx>
+        {`
+          h1 {
+            font-size: 64px;
+          }
+        `}
+      </style>
+    </main>
+  </Layout>
 );
